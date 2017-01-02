b019bf4d7c188d0756e2f79469e04bf1c5742513
JhonDonavan <jhondonavan@gmail.com>
2017-01-02 16:54:28 -0200

    adicionado mensagem de sucesso ao cadastrar titulos

--- src/main/java/com/cobranca/controller/TituloController.java
+++ src/main/java/com/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cobranca.model.Titulo;
 import com.cobranca.repositorio.Titulos;
@@ -11,21 +12,21 @@ import com.cobranca.repositorio.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
-	
+
 	@RequestMapping("/novo")
-	public String novo(){
-		return "CadastroTitulos"; 	
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo){
-		
-		titulos.save(titulo);
-		
+	public String novo() {
 		return "CadastroTitulos";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulos");
+		mv.addObject("mensagen", "Titulo salvo com sucesso");
+		return mv;
 	}
 
 }
