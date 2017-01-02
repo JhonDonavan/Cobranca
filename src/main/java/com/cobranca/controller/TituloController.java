7a9b2223128b08514eebf799b91307f75287ef85
JhonDonavan <jhondonavan@gmail.com>
2017-01-02 17:59:29 -0200

    add comentarios no codigo

--- src/main/java/com/cobranca/controller/TituloController.java
+++ src/main/java/com/cobranca/controller/TituloController.java
@@ -1,8 +1,5 @@
 package com.cobranca.controller;
 
-import java.util.List;
-
-import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
