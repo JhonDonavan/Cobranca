e5952c6904e78fa7ca0fd6ffbe421b9bd7064483
JhonDonavan <jhondonavan@gmail.com>
2017-01-06 20:56:00 -0200

    .

--- src/main/java/com/cobranca/model/Cliente.java
+++ src/main/java/com/cobranca/model/Cliente.java
@@ -14,5 +14,87 @@ public class Cliente {
 	
 	private int dataNascimento;
 	
+	private String endereco;
+	
+	private long cel;
+	
+	private long tel;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(int dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public long getCel() {
+		return cel;
+	}
+
+	public void setCel(long cel) {
+		this.cel = cel;
+	}
+
+	public long getTel() {
+		return tel;
+	}
+
+	public void setTel(long tel) {
+		this.tel = tel;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 	
 }
