b5237b539149f32720b6f3a3bc895b5f411927b7
JhonDonavan <jhondonavan@gmail.com>
2017-01-12 10:56:36 -0200

    Tela novo cliente ok, Rsolvendo agora validações de campo

--- src/main/java/com/cobranca/controller/ClienteController.java
+++ src/main/java/com/cobranca/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.cobranca.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cobranca.model.Cliente;
@@ -21,4 +22,12 @@ public class ClienteController {
 		mv.addObject(new Cliente());
 		return mv;
 	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Cliente cliente){
+		clientes.save(cliente);
+		return "redirect:/clientes/novo";
+	}
+	
 }
