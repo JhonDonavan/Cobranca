7dffa0f07166370701a92b265dec5c43e449ad95
JhonDonavan <jhondonavan@gmail.com>
2017-01-12 14:56:31 -0200

    Menssagens de cadastro de clientes ok e  tela de lista de clientes ok

--- src/main/java/com/cobranca/controller/ClienteController.java
+++ src/main/java/com/cobranca/controller/ClienteController.java
@@ -1,10 +1,15 @@
 package com.cobranca.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cobranca.model.Cliente;
 import com.cobranca.repositorio.Clientes;
@@ -22,12 +27,24 @@ public class ClienteController {
 		mv.addObject(new Cliente());
 		return mv;
 	}
-	
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Cliente cliente){
+	public String salvar(@Validated Cliente cliente, Errors erros, RedirectAttributes attributes) {
+		if (erros.hasErrors()) {
+			return "Cliente/CadastroCliente";
+		}
+
 		clientes.save(cliente);
+		attributes.addFlashAttribute("mensagen", "Cliente salvo com sucesso");
 		return "redirect:/clientes/novo";
 	}
 	
+	@RequestMapping
+	public ModelAndView pesquisar(){
+		List<Cliente> todosClientes = clientes.findAll();
+		ModelAndView mv = new ModelAndView("Cliente/PesquisaClientes");
+		mv.addObject("clientes", todosClientes);
+		return mv;
+	}
+
 }
