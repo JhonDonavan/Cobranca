dcca293fadb11ce74e4c606a1bbb517e3991c40b
JhonDonavan <jhondonavan@gmail.com>
2017-01-14 11:49:53 -0200

    Finalizando exclusao de titulos

---
+++ src/main/resources/static/js/cobranca.js
@@ -0,0 +1,16 @@
+$("#confirmacaoExclusaoModal").on('show.bs.modal', function(event) {
+	
+	var button = $(event.relatedTarget);
+	
+	var idTitulo = button.data('codigo');
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.attr('action');
+	
+	if(!action.endsWith('/')){
+		action += '/';
+	}
+	form.attr('action', action + idTitulo);
+	
+});
