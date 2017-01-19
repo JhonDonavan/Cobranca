cd2e268bec83ee5c11f99b76304f6df0f0b0f570
JhonDonavan <jhondonavan@gmail.com>
2017-01-19 17:43:25 -0200

    Tratando masca de input dos campos de formulario

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -49,37 +49,101 @@ $('#confirmacaoExclusaoModalCliente').on(
 					'tem certeza que deseja ecluir o cliente <strong>'
 							+ nomeCliente + '<strong>?');
 		});
-
+// tudo dentro desta function carrega logo apos o carregamento da pagina
 $(function() {
+	// função para exibir mensagens de incones
 	$('[rel="tooltip"]').tooltip();
+
+	// mascara de campo para valor (MOEDA)
 	$('.js-maskMoney').maskMoney({
 		decimal : ',',
 		thousands : '.',
 		allowZero : true
 	});
 
-	$('.js-atualizar-status').on('click', function(event) {
-		event.preventDefault();
+	// função ajax para alterar status de titulo
+	$('.js-atualizar-status').on(
+			'click',
+			function(event) {
+				event.preventDefault();
 
-		var botaoReceber = $(event.currentTarget);
-		var urlReceber = botaoReceber.attr('href');
-		
-		var response = $.ajax({
-			url : urlReceber,
-			method : 'PUT'
-		});
+				var botaoReceber = $(event.currentTarget);
+				var urlReceber = botaoReceber.attr('href');
 
-		response.done(function(e) {
-			
-			var codigoTitulo = botaoReceber.data('id');
-			$('[data-role=' + codigoTitulo + ']').html('<span class="label label-success">' + e +'<span>');
-			botaoReceber.hide();
-		});
+				var response = $.ajax({
+					url : urlReceber,
+					method : 'PUT'
+				});
 
-		response.fail(function(e) {
-			console.log(e);
-			alert('Erro ao receber cobrança');
-		});
+				response.done(function(e) {
 
-	});
+					var codigoTitulo = botaoReceber.data('id');
+					$('[data-role=' + codigoTitulo + ']')
+							.html(
+									'<span class="label label-success">' + e
+											+ '<span>');
+					botaoReceber.hide();
+				});
+
+				response.fail(function(e) {
+					console.log(e);
+					alert('Erro ao receber cobrança');
+				});
+
+			});
 })
+
+$(document).ready(function() {
+
+	
+	$(function() {
+        $.mask.definitions['~'] = "[+-]";
+        $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy",completed:function(){alert("completed!");}});
+        $(".phone").mask("(999) 999-9999");
+        $(".cel").mask("(999) 99999-9999");
+        $("#phoneExt").mask("(999) 999-9999? x99999");
+        $("#iphone").mask("+33 999 999 999");
+        $("#tin").mask("99-9999999");
+        $("#ssn").mask("999-99-9999");
+        $("#product").mask("a*-999-a999", { placeholder: " " });
+        $("#eyescript").mask("~9.99 ~9.99 999");
+        $("#po").mask("PO: aaa-999-***");
+        $("#pct").mask("99%");
+        $("#phoneAutoclearFalse").mask("(999) 999-9999", { autoclear: false, completed:function(){alert("completed autoclear!");} });
+        $("#phoneExtAutoclearFalse").mask("(999) 999-9999? x99999", { autoclear: false });
+        $("input").blur(function() {
+            $("#info").html("Unmasked value: " + $(this).mask());
+        }).dblclick(function() {
+            $(this).unmask();
+        });
+    });
+
+	$('#dataTable').dataTable({
+		"language" : {
+			"sEmptyTable" : "Nenhum registro encontrado",
+			"sInfo" : "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+			"sInfoEmpty" : "Mostrando 0 até 0 de 0 registros",
+			"sInfoFiltered" : "(Filtrados de _MAX_ registros)",
+			"sInfoPostFix" : "",
+			"sInfoThousands" : ".",
+			"sLengthMenu" : "_MENU_ resultados por página",
+			"sLoadingRecords" : "Carregando...",
+			"sProcessing" : "Processando...",
+			"sZeroRecords" : "Nenhum registro encontrado",
+			"sSearch" : "Pesquisar",
+			"oPaginate" : {
+				"sNext" : "Próximo",
+				"sPrevious" : "Anterior",
+				"sFirst" : "Primeiro",
+				"sLast" : "Último"
+			},
+			"oAria" : {
+				"sSortAscending" : ": Ordenar colunas de forma ascendente",
+				"sSortDescending" : ": Ordenar colunas de forma descendente"
+			}
+
+		}
+	});
+
+});
+
